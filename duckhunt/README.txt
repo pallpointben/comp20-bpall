0434e5ea6be6ed95e1179e20f470c95dac38966a
Ben Pall <pallpointben@gmail.com>
2015-10-13 03:55:35 -0400

    finished duck hunt lab

---
+++ duckhunt/README.txt
@@ -0,0 +1,3 @@
+I have correctly implemented drawing the background and the two ducks on the canvas.
+
+This assignment took me approximately one hour. I collaborated with no one.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+
+function init() {
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+	var background = new Image(256, 240);
+	background.src = "duckhunt-background.gif";
+	var ducks = new Image(375, 276); 
+	ducks.src = "duckhunt_various_sheet.png";
+	background.onload = function(){
+		ctx.drawImage(background, 0, 0, 550, 400);
+	}
+	ducks.onload = function(){
+		ctx.drawImage(ducks, 0, 120, 32, 32, 200, 128, 32, 32);
+		ctx.drawImage(ducks, 128, 152, 32, 32, 350, 170, 32, 32);
+	}
+}
