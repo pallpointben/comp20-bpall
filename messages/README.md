fcf67c970dfa216d2674e90ee00ba8270816c8b3
Ben Pall <pallpointben@gmail.com>
2015-10-21 02:50:10 -0400

    finished lab 6

---
+++ messages/README.md
@@ -0,0 +1,7 @@
+# Comp 20 Lab 6
+
+# The entire lab has been implemented correctly.
+
+# I did not collaborate with anyone.
+
+# This assignment took me approximately 2 hours to complete.
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,32 @@
-// Your JavaScript goes here...
+var request = new XMLHttpRequest();
+var textarray = "";
+
+function parse() {
+	request.open("GET", 'data.json', true);
+	request.send(null);
+
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+      	 	textarray = JSON.parse(request.responseText);
+			displayMessages(textarray);
+    	}
+	}
+}
+
+
+function displayMessages(arr) {
+	var output = "";
+	var i;
+	for (i = 0; i < arr.length; i++) {
+		output += 
+		"<div class='messages'> <p class='author'>" + 
+				arr[i].username + 
+			"</p> <p class='message'>" + 
+				arr[i].content + 
+			"</p> </div>"
+	}
+	messagedisplay = document.getElementById("messages");
+	messagedisplay.innerHTML = output;
+
+}
